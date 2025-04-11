bccd0be2c21bc27797d1818026938ba1be4b448e
sujanreshmi36 <sujan.reshmi.7@gmail.com>
2025-04-11 11:20:11 +0545

    updated image cost

--- src/helper/utils/get-tokencost.ts
+++ src/helper/utils/get-tokencost.ts
@@ -1,37 +1,20 @@
 export class CalculateUsedToken {
-    // getTokenCost(imageSize: string, model: string) {
-    //     const costMap = {
-    //         "256x256": 50,
-    //         "512x512": 100,
-    //         "1024x1024": model === "dall-e-3" ? 1000 : 500,
-    //         "1792x1024": 1500,
-    //         "1024x1792": 1500
-    //     };
-    //     return costMap[imageSize] || 0; // Returns token cost for the given size and model
-    // }
-
     getTokenCost(imageSize: string, model: string): number {
-        // const baseCostMap = {
-        //     "256x256": 5000,   // Base cost
-        //     "512x512": 75000,   // Base cost
-        //     "1024x1024": model === "dall-e-3" ? 160000 : 85000, // Base cost
-        //     "1792x1024": 320000, // Base cost
-        //     "1024x1792": 320000  // Base cost
-        // };
         const baseCostMap = {
-            "1024x1024": 0.08, // Base cost
-            "1024x1792": 0.12  // Base cost
+            "1024x1024": 0.08, // 1:1
+            "1024x1792": 0.12, // Portrait
+            "1792x1024": 0.12  // Landscape
         };
 
-        // Apply a 20% profit margin
-        const profitMargin = 0.15;
-        const totalCostPerMillionTokens = Number(process.env.TOTALTOKENCOST);
+        const baseCost = baseCostMap[imageSize];
+        if (!baseCost) return 0;
 
-        const token = ((baseCostMap[imageSize] * profitMargin) / totalCostPerMillionTokens) * 1000000
+        const profitMargin = Number(process.env.PROFIT_MARGIN) || 0.15;
+        const costPerMillionTokens = Number(process.env.TOTALTOKENCOST) || 1;
 
+        const profitAddedCost = baseCost + (baseCost * profitMargin);
+        const tokenCost = (profitAddedCost / costPerMillionTokens) * 1_000_000;
 
-        // return Math.ceil(baseCostMap[imageSize] * profitMargin);
-        return Math.floor(token);
+        return Math.floor(tokenCost);
     }
 }
-
